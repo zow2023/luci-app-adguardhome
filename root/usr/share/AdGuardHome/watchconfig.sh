ee798df93470a714f41450c8bbcb8a92d3f1b875
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-09-06 17:20:37 +0800

    Refactor watchconfig.sh for config path handling

--- root/usr/share/AdGuardHome/watchconfig.sh
+++ root/usr/share/AdGuardHome/watchconfig.sh
@@ -3,16 +3,21 @@
 
 PATH="/usr/sbin:/usr/bin:/sbin:/bin"
 
-configpath=$(uci -q get adguardhome.config.config_file)
-[ -z "$configpath" ] && configpath="/etc/adguardhome/adguardhome.yaml"
-
-while :
-do
+while :; do
 	sleep 10
-	if [ -f "$configpath" ]; then
-		/etc/init.d/adguardhome do_redirect 1
+
+	configpath="$(uci -q get adguardhome.config.config_file)"
+	[ -n "$configpath" ] || \
+		configpath="/etc/adguardhome/adguardhome.yaml"
+
+	[ -f "$configpath" ] || continue
+
+	if /etc/init.d/adguardhome do_redirect 1; then
 		break
 	fi
+
+	logger -t adguardhome \
+		"watchconfig: failed to apply redirect for $configpath, retrying"
 done
 
 exit 0
